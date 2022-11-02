3f43072b275d0dadb2e1bb4e6633f536e3cb98a8
technichron <sethpoulsen@gmail.com>
2022-11-02 18:03:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from functiondef import *
 import pygame as pg
  
 def main():
-    
+
     pg.init()
     pg.display.set_icon(pg.image.load("widechora.png"))
     pg.display.set_caption("RTX 500000 RAYTRACED 16K GAMING")
-    screen = pg.display.set_mode((1000,500))
+    screen = pg.display.set_mode((1000,500), pg.RESIZABLE)
     width, height = pg.display.get_window_size()
+
     def w(coord):
-        return coord*width
+        return int(coord*width)
     def h(coord):
-        return coord*height
+        return int(coord*height)
 
 
     running = True
@@ -20,18 +21,24 @@ def main():
 
     # main loop
     while running:
+        screen.fill((10,10,10))
         width, height = pg.display.get_window_size()
+
+
+
+
+
+
+
+
 
         # event handling, gets all event from the event queue
         for event in pg.event.get():
             
             if event.type == pg.QUIT:
                 running = False
-     
-     
 
-
-
+        pg.display.update()
 
 
 if __name__=="__main__":
