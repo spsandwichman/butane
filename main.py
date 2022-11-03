4964710328b86ac47773154ce9f53d2d40fa5c94
technichron <sethpoulsen@gmail.com>
2022-11-03 16:01:48 -0400

    simple refactoring
    > refactored to be less pygame-centric, replacing the main screen with a numpy array
    + gitignore .pyc cache files

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from functiondef import *
 import pygame as pg
 import numpy as np
- 
+
 def main():
+
+    resolution = (1000,500)
+    width, height = resolution
+    screen = np.full((width,height,3), 0, dtype=np.uint8)
+
 
     pg.init()
     pg.display.set_icon(pg.image.load("widechora.png"))
     pg.display.set_caption("RTX 500000 RAYTRACED 16K GAMING")
-    screen = pg.display.set_mode((1000,500), pg.RESIZABLE)
-    width, height = pg.display.get_window_size()
+    pgscreen = pg.display.set_mode(resolution)
+    
 
     camera = Camera()
     
@@ -19,31 +24,39 @@ def main():
         return int(coord*height)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     running = True
-    
-    
-
-    # main loop
+    # display loop
     while running:
-        screen.fill((10,10,10))
-        width, height = pg.display.get_window_size()
-
-
-
-
-
-
-
-
-
-        # event handling, gets all event from the event queue
+        pg.surfarray.blit_array(pgscreen, screen)
         for event in pg.event.get():
-            
             if event.type == pg.QUIT:
                 running = False
-
         pg.display.update()
-
 
 if __name__=="__main__":
     main()
